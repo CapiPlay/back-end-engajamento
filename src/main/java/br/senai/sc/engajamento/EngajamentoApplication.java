d0233f1ddedb6e2ee9b0c8c8369841448f1a5cc8
Lucas <lucas_a_mader@estudante.sesisenai.org.br>
2023-06-27 18:33:07 -0300

    build_engajamento

---
+++ src/main/java/br/senai/sc/engajamento/EngajamentoApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sc.engajamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngajamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngajamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senai/sc/engajamento/EngajamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.sc.engajamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngajamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
