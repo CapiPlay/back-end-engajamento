21ef28e11ea0e045cad3acb449d8a00480fcd928
Lucas <lucas_a_mader@estudante.sesisenai.org.br>
2023-07-04 18:56:44 -0300

    model-begin

--- src/main/java/br/senai/sc/engajamento/model/entity/Historico.java
+++ src/main/java/br/senai/sc/engajamento/model/entity/Historico.java
@@ -1,5 +1,23 @@
 package br.senai.sc.engajamento.model.entity;
 
-public class Historico {
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import java.util.UUID;
+import lombok.Data;
 
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "THistorico")
+public class Historico {
+    @Id
+    private UUID uuid;
+    @ManyToOne
+    @Column(nullable = false)
+    private Usuario usuario;
+    @ManyToOne
+    @Column(nullable = false)
+    private Video video;
 }
--- src/main/java/br/senai/sc/engajamento/model/entity/Video.java
+++ src/main/java/br/senai/sc/engajamento/model/entity/Video.java
@@ -1,5 +1,23 @@
 package br.senai.sc.engajamento.model.entity;
 
-public class Video {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TVideo")
+public class Video {
+    @Id
+    private UUID uuid;
+    @Column(nullable = false)
+    private Long visualizacao;
 }
