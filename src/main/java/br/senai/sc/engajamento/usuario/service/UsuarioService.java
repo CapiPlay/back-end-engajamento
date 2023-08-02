b62ffd8bdec189c5f4369e9ab759fa07b91b1925
Lucas <lucas_a_mader@estudante.sesisenai.org.br>
2023-08-01 21:24:04 -0300

    inscricaoTest working

--- src/main/java/br/senai/sc/engajamento/usuario/service/UsuarioService.java
+++ src/main/java/br/senai/sc/engajamento/usuario/service/UsuarioService.java
@@ -1,13 +1,9 @@
 package br.senai.sc.engajamento.usuario.service;
 
-import br.senai.sc.engajamento.exception.NaoEncontradoException;
 import br.senai.sc.engajamento.usuario.model.entity.Usuario;
 import br.senai.sc.engajamento.usuario.repository.UsuarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -15,10 +11,6 @@ public class UsuarioService {
     private final UsuarioRepository repository;
 
     public Usuario retornaUsuario(String idUsuario) {
-        Optional<Usuario> optionalUsuario = repository.findById(idUsuario);
-        if(optionalUsuario.isPresent()){
-            return optionalUsuario.get();
-        }
-        throw new NaoEncontradoException("Usuário encontrado");
+        return repository.findById(idUsuario).orElseThrow();
     }
 }
